af166f5dcff7cae2fffe35c89654f69204a007c1
Dana Khaing <WLIS410@live.rhul.ac.uk>
2026-04-16 10:52:56 +0100

    Add Google auth configuration support

--- flowbit-backend/flowbit_backend/settings.py
+++ flowbit-backend/flowbit_backend/settings.py
@@ -135,6 +135,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOW_ALL_ORIGINS = True # dev only - restrict later
+GOOGLE_OAUTH_CLIENT_ID = config('GOOGLE_OAUTH_CLIENT_ID', default='')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
