35d648e72693359560c0dad0f39d69935abb88f0
Dana Khaing <dana1352000.uk@gmail.com>
2026-04-19 13:04:42 +0100

    Add reusable ui primitives for auth flows

--- flowbit-frontend/src/app/layout.tsx
+++ flowbit-frontend/src/app/layout.tsx
@@ -1,6 +1,10 @@
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import type { Metadata } from "next";
 import { Fraunces, Inter } from "next/font/google";
 import "./globals.css";
+
+config.autoAddCss = false;
 
 const inter = Inter({
   variable: "--font-inter",
