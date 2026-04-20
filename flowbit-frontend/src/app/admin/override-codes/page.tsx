3ab3582b3025d8946a1f9d7b06aaf38124e592ba
Dana Khaing <WLIS410@live.rhul.ac.uk>
2026-04-20 20:12:48 +0100

    Split override management into dedicated admin page

---
+++ flowbit-frontend/src/app/admin/override-codes/page.tsx
@@ -0,0 +1,5 @@
+import { AdminOverrideCodesPage } from "@/components/admin/admin-override-codes-page";
+
+export default function OverrideCodesAdminRoute() {
+  return <AdminOverrideCodesPage />;
+}
