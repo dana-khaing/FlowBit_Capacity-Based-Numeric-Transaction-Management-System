ec9f74d1a85b70fe88d3e256a24d2b0f90ff505e
Dana Khaing <WLIS410@live.rhul.ac.uk>
2026-05-15 02:57:57 +0100

    Fix Channels ASGI import order
    Co-authored-by: Khant Zayar <khant.zayar.dev@gmail.com>

--- flowbit-backend/flowbit_backend/asgi.py
+++ flowbit-backend/flowbit_backend/asgi.py
@@ -14,10 +14,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flowbit_backend.settings")
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+django_asgi_app = get_asgi_application()
+
 from core.notification_realtime import TokenAuthMiddleware
 from core.routing import websocket_urlpatterns
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
